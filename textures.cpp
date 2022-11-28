b22b06f10cd76138d841c0804b3eb6029442d6d6
Markus Rytter <markus.r@live.dk>
2022-11-28 22:13:54 +0100

    Allocating memory for texture

--- textures.cpp
+++ textures.cpp
@@ -1,20 +1,22 @@
 #include "textures.hpp"
 
-sf::Texture grass_texture;
 sf::Sprite grass_sprite;
 
-void load_texture(sf::Texture *texture, sf::Sprite *sprite, char *texture_path)
+void load_texture(sf::Sprite *sprite, char *texture_path)
 {
-    if (!texture->loadFromFile(texture_path, sf::IntRect(0, 0, 16, 16)))
+    // Isn't freed until program is closed
+    sf::Texture * texture = (sf::Texture*) malloc(sizeof(sf::Texture));
+    if (!texture->loadFromFile(texture_path, sf::IntRect(GRASS1_NORTHWEST_INNER_CORNER)))
     {
         std::cout << "Error loading texture from " << texture_path << std::endl;
     }
     sprite->setTexture(*texture);
+    sprite->setScale(10, 10);
 }
 
 void init_textures()
 {
-    load_texture(&grass_texture, &grass_sprite, "Assets/Terrain/grass_tileset_16x16.png");
+    load_texture(&grass_sprite, "Assets/Terrain/grass_tileset_16x16.png");
 }
 
 sf::Sprite get_grass_sprite()

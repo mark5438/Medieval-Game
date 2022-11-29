e354c29514a694c83479874e740272879bf42ed8
Markus Rytter <markus.r@live.dk>
2022-11-29 17:04:45 +0100

    Removed unused files

--- main.cpp
+++ main.cpp
@@ -1,12 +1,10 @@
 #include <SFML/Graphics.hpp>
 
-#include "src/textures.hpp"
 #include "src/map_loader.hpp"
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(1600, 800), "Medieval Game");
-    init_textures();
 
     Map map = Map::load_map("Tiled/starting_map.tmx");
 
@@ -20,8 +18,8 @@ int main()
         }
 
         window.clear();
-        
-        window.draw(get_sprite(45));
+
+
 
         window.display();
     }

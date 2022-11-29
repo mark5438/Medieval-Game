ed9aac1e380eab8dbb600189b88d1914fec7b962
Markus Rytter <markus.r@live.dk>
2022-11-29 12:24:52 +0100

    Created tiled project

---
+++ Tiled/Fence.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Fence" tilewidth="32" tileheight="32" tilecount="2048" columns="16">
+ <image source="../Assets/Terrain/fence_medieval.png" width="512" height="1024"/>
+</tileset>

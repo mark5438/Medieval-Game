7e66ca4f34b666f4de103f8aae5ce79e03e00110
Markus Rytter <markus.r@live.dk>
2022-11-29 13:12:33 +0100

    Parsing simple map meta data

--- map_loader.cpp
+++ map_loader.cpp
@@ -1,6 +1,33 @@
 #include "map_loader.hpp"
 
-Map load_map(char* file_path)
+Map::Map(const char *file_path)
 {
-    
+    this->file_path = file_path;
+    this->load_xml_document();
+
+    this->width = this->get_integer_attribute("width", 5);
+    this->height = this->get_integer_attribute("height", 6);
+    this->tile_width = this->get_integer_attribute("tilewidth", 9);
+    this->tile_height = this->get_integer_attribute("tileheight", 10);
+}
+
+void Map::load_xml_document()
+{
+    std::ifstream map_file(file_path);
+    std::vector<char> buffer((std::istreambuf_iterator<char>(map_file)), std::istreambuf_iterator<char>());
+    buffer.push_back('\0');
+
+    this->doc.parse<0>(&buffer[0]);
+
+    this->root_node = doc.first_node("map");
+}
+
+int Map::get_integer_attribute(const char* attribute, int size)
+{
+    return atoi(this->root_node->first_attribute(attribute, size)->value());
+}
+
+Map Map::load_map(const char *file_path)
+{
+    return Map(file_path);
 }
--- map_loader.hpp
+++ map_loader.hpp
@@ -5,6 +5,29 @@
 
 class Map
 {
-};
+private:
+    unsigned int width;
+    unsigned int height;
+    unsigned int tile_width;
+    unsigned int tile_height;
 
-Map load_map(char* file_path);
+    const char *file_path;
+    rapidxml::xml_document<> doc;
+    rapidxml::xml_node<> *root_node = NULL;
+
+    void load_xml_document();
+    int get_integer_attribute(const char *, int);
+
+public:
+    /*
+     *   A map is initialized with a width, height,
+     *   tile_width and tile_height
+     */
+    Map(const char *);
+    static Map load_map(const char *file_path);
+
+    unsigned int get_width() { return this->width; }
+    unsigned int get_height() { return this->height; }
+    unsigned int get_tile_width() { return this->tile_width; }
+    unsigned int get_tile_height() { return this->tile_height; }
+};

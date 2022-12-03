42530353748fe8d7f0d0044b576309a267045d65
Markus Rytter <markus.r@live.dk>
2022-12-03 12:15:19 +0100

    Added camera functions

---
+++ src/camera/camera.cpp
@@ -0,0 +1,51 @@
+#include "camera.hpp"
+
+int x, y;
+float zoom;
+
+int get_camera_x()
+{
+    return x;
+}
+
+int get_camera_y()
+{
+    return y;
+}
+
+float get_camera_zoom()
+{
+    return zoom;
+}
+
+int move_camera_x(int delta)
+{
+    x += delta;
+    return x;
+}
+
+int move_camera_y(int delta)
+{
+    y += delta;
+    return y;
+}
+
+float change_camera_zoom(float delta)
+{
+    zoom += delta;
+}
+
+void set_camera_x(int x_)
+{
+    x = x_;
+}
+
+void set_camera_y(int y_)
+{
+    y = y_;
+}
+
+void set_camera_zoom(float zoom_)
+{
+    zoom = zoom_;
+}
---
+++ src/camera/camera.hpp
@@ -0,0 +1,11 @@
+int get_camera_x();
+int get_camera_y();
+float get_camera_zoom();
+
+int move_camera_x(int);
+int move_camera_y(int);
+float change_camera_zoom(float);
+
+void set_camera_x(int);
+void set_camera_y(int);
+void set_camera_zoom(float);

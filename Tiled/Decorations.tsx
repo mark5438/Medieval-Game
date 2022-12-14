7c672d9d70aa5c773bb5a5f09aaddf8ddfa5abb0
Markus Rytter <markus.r@live.dk>
2022-12-14 11:02:40 +0100

    Map changes

--- Tiled/Decorations.tsx
+++ Tiled/Decorations.tsx
@@ -1,4 +1,76 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.2" name="Decorations" tilewidth="16" tileheight="16" tilecount="4096" columns="32">
  <image source="../Assets/Terrain/decorations-medieval.png" width="512" height="2048"/>
+ <tile id="3024">
+  <animation>
+   <frame tileid="3024" duration="250"/>
+   <frame tileid="3026" duration="250"/>
+   <frame tileid="3028" duration="250"/>
+   <frame tileid="3030" duration="250"/>
+   <frame tileid="3032" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="3025">
+  <animation>
+   <frame tileid="3025" duration="250"/>
+   <frame tileid="3027" duration="250"/>
+   <frame tileid="3029" duration="250"/>
+   <frame tileid="3031" duration="250"/>
+   <frame tileid="3033" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="3056">
+  <animation>
+   <frame tileid="3056" duration="250"/>
+   <frame tileid="3058" duration="250"/>
+   <frame tileid="3060" duration="250"/>
+   <frame tileid="3062" duration="250"/>
+   <frame tileid="3064" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="3057">
+  <animation>
+   <frame tileid="3057" duration="250"/>
+   <frame tileid="3059" duration="250"/>
+   <frame tileid="3061" duration="250"/>
+   <frame tileid="3063" duration="250"/>
+   <frame tileid="3065" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="3088">
+  <animation>
+   <frame tileid="3088" duration="250"/>
+   <frame tileid="3090" duration="250"/>
+   <frame tileid="3092" duration="250"/>
+   <frame tileid="3094" duration="250"/>
+   <frame tileid="3096" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="3089">
+  <animation>
+   <frame tileid="3089" duration="250"/>
+   <frame tileid="3091" duration="250"/>
+   <frame tileid="3093" duration="250"/>
+   <frame tileid="3095" duration="250"/>
+   <frame tileid="3097" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="3120">
+  <animation>
+   <frame tileid="3120" duration="250"/>
+   <frame tileid="3122" duration="250"/>
+   <frame tileid="3124" duration="250"/>
+   <frame tileid="3126" duration="250"/>
+   <frame tileid="3128" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="3121">
+  <animation>
+   <frame tileid="3121" duration="250"/>
+   <frame tileid="3123" duration="250"/>
+   <frame tileid="3125" duration="250"/>
+   <frame tileid="3127" duration="250"/>
+   <frame tileid="3129" duration="250"/>
+  </animation>
+ </tile>
 </tileset>

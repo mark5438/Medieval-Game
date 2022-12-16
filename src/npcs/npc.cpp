d78826d099929ba834e39dc0d7b4e3c94dd49d08
Markus Rytter <markus.r@live.dk>
2022-12-16 10:56:02 +0100

    Added new interface class NPC_Animation and generic animal implementation

--- src/npcs/npc.cpp
+++ src/npcs/npc.cpp
@@ -5,3 +5,11 @@ NPC::NPC(int x, int y)
     this->position.x = x;
     this->position.y = y;
 }
+
+NPC_Animation_Generic_Animal::NPC_Animation_Generic_Animal(int w, int n, int e, int s)
+{
+    this->walk_west_tile = w;
+    this->walk_north_tile = n;
+    this->walk_east_tile = e;
+    this->walk_south_tile = s;
+}
--- src/npcs/npc.hpp
+++ src/npcs/npc.hpp
@@ -5,18 +5,50 @@
 #ifndef NPC_TYPE
 #define NPC_TYPE
 
+/**
+ * Interface class for all NPC animations. Each NPC objects hold an object of this type,
+ * That it uses to get the right animations.
+ *
+ * For each NPC a subclass of NPC_Animation is needed
+ */
+class NPC_Animation
+{
+public:
+    virtual int get_walking_west_sprite_id() = 0;
+    virtual int get_walking_north_sprite_id() = 0;
+    virtual int get_walking_east_sprite_id() = 0;
+    virtual int get_walking_south_sprite_id() = 0;
+};
+
+class NPC_Animation_Generic_Animal : NPC_Animation
+{
+private:
+    int walk_west_tile;
+    int walk_north_tile;
+    int walk_east_tile;
+    int walk_south_tile;
+
+public:
+    NPC_Animation_Generic_Animal(int, int, int, int);
+    int get_walking_west_sprite_id() { return this->walk_west_tile; }
+    int get_walking_north_sprite_id() { return this->walk_north_tile; }
+    int get_walking_east_sprite_id() { return this->walk_east_tile; }
+    int get_walking_south_sprite_id() { return this->walk_south_tile; }
+};
+
 class NPC
 {
 private:
     sf::Vector2<int> position;
     sf::Vector2<int> tile_offset;
 
+    NPC_Animation animation;
+
 public:
     NPC(int, int);
     int get_sprite_id() { return CHICKEN_NPC_NUMBER; }
     int get_x() { return this->position.x; }
     int get_y() { return this->position.y; }
-
 };
 
 #endif // NPC_TYPE
